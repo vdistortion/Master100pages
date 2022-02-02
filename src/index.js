eca623342a0a311849a306558c513dea9107621c
valentin <astrotrain55@gmail.com>
2022-02-03 00:03:47 +0500

    add pdf

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const path = require('path');
 const fs = require('fs');
 
 const input = fs.createReadStream(path.resolve(__dirname, 'input.tex'));
-const output = fs.createWriteStream(path.resolve(__dirname, 'output.pdf'));
+const output = fs.createWriteStream(path.resolve(__dirname, '..', 'master.pdf'));
 const pdf = latex(input);
 
 pdf.pipe(output);
-pdf.on('error', (err) => console.error(err));
+pdf.on('error', console.error);
 pdf.on('finish', () => console.log('PDF generated!'));
